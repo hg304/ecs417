d24a42a6311f33bc95a95bccbbca2bc7e3685ff1
Hasib <ec19051@qmul.ac.uk>
2020-05-18 16:08:28 +0100

    changed

--- webroot/miniproject/addPost.php
+++ webroot/miniproject/addPost.php
@@ -1,13 +1,12 @@
 <?php
 
-include("config.php");
-
+include("config.php")
 if (isset($_POST["submit"]))
 {
   $title = $_POST["title"];
   $description = $_POST["description"];
 
-  $res = mysqli_query($conn, "INSERT INTO BLOG (title, description, datepost) values ('$title', '$description', now());");
+  $res = mysqli_query($conn, "INSERT INTO BLOG (title, description, datepost) VALUES ('$title', '$description', now());");
 
   header("Location:viewBlog.php");
 }
