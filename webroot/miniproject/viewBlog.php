30a86b3bd08dd58ecd258d7ea208ac8b384f3c80
Hasib <ec19051@qmul.ac.uk>
2020-05-18 22:14:50 +0100

    changed

--- webroot/miniproject/viewBlog.php
+++ webroot/miniproject/viewBlog.php
@@ -22,11 +22,46 @@
     </ul>
   </nav>
 
-  <section>
+  <section class="topme">
     <article class="add">
-    <button type="button" onclick="location.href='addPost.html'">Add post</button>
-  </article>
-</section><br>
+      <button type="button" onclick="location.href='addPost.html'">Add post</button>
+    </article>
+    <article class="search">
+      <form id="monthpick" method="POST" action="viewBlog.php">
+        <label for="months">Select month</label>
+        <select class="months">
+          <option value="1">January</option>
+          <option value="2">February</option>
+          <option value="3">March</option>
+          <option value="4">April</option>
+          <option value="5">May</option>
+          <option value="6">June</option>
+          <option value="7">July</option>
+          <option value="8">August</option>
+          <option value="9">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
+        </select>
+        <button type="button" onclick="checkempty()" value="submit">Search</button>
+      </form>
+      <script>
+        function checkempty()
+        {
+          var form = document.getElementById("monthpick");
+          var month = document.getElementById("months");
+
+          if (month.value.length > 0)
+          {
+            form.submit();
+          }
+          else
+          {
+            e.preventDefault();
+          }
+        }
+      </script>
+  </section><br>
 
   <section>
     <h2 style="font-family: Tahoma;">Recent blog posts:</h2>
@@ -34,28 +69,58 @@
     <?php
       include("config.php");
 
-      $res = mysqli_query($conn, "SELECT * FROM BLOG;");
-      if (!$res)
-       {
-        printf("Error: %s\n", mysqli_error($conn));
-        exit();
+      $month = $_POST["months"];
+
+      if ($month == null)
+      {
+        $res = mysqli_query($conn, "SELECT * FROM BLOG;");
+        if (!$res)
+         {
+          printf("Error: %s\n", mysqli_error($conn));
+          exit();
+         }
+
+         $rows = mysqli_num_rows($res);
+
+         for ($i = $rows; $i > 0; $i--)
+         {
+           $res2 = mysqli_query($conn, "SELECT * FROM BLOG WHERE id = '$i';");
+           if (!$res2)
+            {
+             printf("Error: %s\n", mysqli_error($conn));
+             exit();
+            }
+
+           $result = mysqli_fetch_array($res2);
+
+           echo "<h3><style='font-family: Tahoma; text-align: center; color: white;'>", $result["title"], "</h3><p><style='color: white; font-family: Tahoma;'>", $result[description], "</p><p><br><small style='color:white; font-family:Tahoma;'>", date("d/m/y g:i A", strtotime($result["datepost"])), "</small></p><hr>";
+         }
        }
-
-       $rows = mysqli_num_rows($res);
-
-       for ($i = $rows; $i > 0; $i--)
+       else
        {
-         $res2 = mysqli_query($conn, "SELECT * FROM BLOG WHERE id = '$i';");
-         if (!$res2)
+         $res = mysqli_query($conn, "SELECT * FROM BLOG WHERE MONTH(postdate) = ;");
+         if (!$res)
           {
            printf("Error: %s\n", mysqli_error($conn));
            exit();
           }
 
-         $result = mysqli_fetch_array($res2);
+          $rows = mysqli_num_rows($res);
 
-         echo "<h3><style='font-family: Tahoma; text-align: center; color: white;'>", $result["title"], "</h3><p><style='color: white; font-family: Tahoma;'>", $result[description], "</p><p><br><small style='color:white; font-family:Tahoma;'>", date("d/m/y g:i A", strtotime($result["datepost"])), "</small></p><hr>";
-       }
+          for ($i = $rows; $i > 0; $i--)
+          {
+            $res2 = mysqli_query($conn, "SELECT * FROM BLOG WHERE id = '$i';");
+            if (!$res2)
+             {
+              printf("Error: %s\n", mysqli_error($conn));
+              exit();
+             }
+
+            $result = mysqli_fetch_array($res2);
+
+            echo "<h3><style='font-family: Tahoma; text-align: center; color: white;'>", $result["title"], "</h3><p><style='color: white; font-family: Tahoma;'>", $result[description], "</p><p><br><small style='color:white; font-family:Tahoma;'>", date("d/m/y g:i A", strtotime($result["datepost"])), "</small></p><hr>";
+          }
+        }
      ?>
    </article>
    </section>
