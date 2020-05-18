68e3ca79c2437334a805084e11b194e6d8c95705
Hasib <ec19051@qmul.ac.uk>
2020-05-18 17:34:51 +0100

    changed

--- webroot/miniproject/addPost.php
+++ webroot/miniproject/addPost.php
@@ -8,6 +8,11 @@ if (isset($_POST["submit"]))
   $description = $_POST["description"];
 
   $res = mysqli_query($conn, "INSERT INTO BLOG (title, description, datepost) VALUES ('$title', '$description', now());");
+  if (!$res)
+   {
+    printf("Error: %s\n", mysqli_error($conn));
+    exit();
+   }
 
   header("Location:viewBlog.php");
 }
