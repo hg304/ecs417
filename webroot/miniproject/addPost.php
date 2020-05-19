5dc186d95eaf137c921eddf0b9deb8392a180022
Hasib <ec19051@qmul.ac.uk>
2020-05-19 21:49:09 +0100

    changed

--- webroot/miniproject/addPost.php
+++ webroot/miniproject/addPost.php
@@ -1,39 +1,14 @@
 <?php
 
-session_start();
-include("config.php");
-
-if ($_POST["submit"] == "preview")
-{
-  $title = $_POST["title"];
-  $description = $_POST["description"];
-
-  $_SESSION["title"] = $title;
-  $_SESSION["description"] = $description;
-
-  $res = mysqli_query($conn, "INSERT INTO BLOG (title, description, datepost) VALUES ('$title', '$description', now());");
-  if (!$res)
-   {
-    printf("Error: %s\n", mysqli_error($conn));
-    exit();
-   }
-
-  header("Location:preview.php");
-}
-else if ($_POST["submit"] == "submit")
-{
   $title = $_POST["title"];
   $description = $_POST["description"];
 
   $res = mysqli_query($conn, "INSERT INTO BLOG (title, description, datepost) VALUES ('$title', '$description', now());");
   if (!$res)
-   {
+  {
     printf("Error: %s\n", mysqli_error($conn));
     exit();
-   }
+  }
 
   header("Location:viewBlog.php");
-}
-
-
 ?>
