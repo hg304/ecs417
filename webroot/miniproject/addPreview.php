d538e0d485edf9c64da9e40d913b9b3193e829e8
Hasib <ec19051@qmul.ac.uk>
2020-05-20 04:17:20 +0100

    changed

--- webroot/miniproject/addPreview.php
+++ webroot/miniproject/addPreview.php
@@ -11,6 +11,7 @@
   $_SESSION["description"] = $description;
 
   $res = mysqli_query($conn, "INSERT INTO BLOG (title, description, datepost) VALUES ('$title', '$description', now());");
+
   if (!$res)
   {
     printf("Error: %s\n", mysqli_error($conn));
