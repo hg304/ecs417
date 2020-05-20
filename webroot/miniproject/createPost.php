9d70faa7f3ddaf3f49ee5207e393399b570ed2b1
Hasib <ec19051@qmul.ac.uk>
2020-05-20 09:17:36 +0100

    changed

--- webroot/miniproject/createPost.php
+++ webroot/miniproject/createPost.php
@@ -29,6 +29,7 @@ session_start();
           <input type="text" id="title" placeholder="Title" name="title" <?php echo "value=", $_SESSION["title"]?>><br><br>
           <textarea id="description" name="description" placeholder="Write anything here" <?php echo "value=", $_SESSION["description"]?>></textarea>
           <br><br><button type="submit" id="submit" name="submit" value="submit">Submit</button>
+          <button type="submit" id="preview" name="preview" formaction="preview.php" value="submit">Submit</button>
           <button type="button" onclick="resetText()" value="reset">Clear</button>
           <script>
 
@@ -43,6 +44,7 @@ session_start();
                 var title = document.getElementById("title");
                 var desc = document.getElementById("description");
                 var subbutton = document.getElementById("submit");
+                var prevbutton = document.getElementById("preview");
 
                 subbutton.addEventListener("click", function(e){
                   if (title.value.length > 0)
@@ -67,7 +69,29 @@ session_start();
                   }
                 }, false);
 
-
+                prevbutton.addEventListener("click", function(e){
+                  if (title.value.length > 0)
+                  {
+                    title.style.borderColor = "white";
+                  }
+                  else
+                  {
+                    alert("Both fields have not been filled in");
+                    title.style.borderColor = "red";
+                    e.preventDefault();
+                  }
+                  if (desc.value.length > 0)
+                  {
+                    desc.style.borderColor = "white";
+                  }
+                  else
+                  {
+                    alert("Both fields have not been filled in");
+                    desc.style.borderColor = "red";
+                    e.preventDefault();
+                  }
+                }, false);
+                
           </script>
     </<fieldset>
    </form>
