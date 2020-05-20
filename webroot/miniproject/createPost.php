4a36fd2cc723629ad569dfa2df5374e09a32e969
Hasib <ec19051@qmul.ac.uk>
2020-05-20 09:12:18 +0100

    changed

--- webroot/miniproject/createPost.php
+++ webroot/miniproject/createPost.php
@@ -28,7 +28,7 @@ session_start();
     <fieldset class="blog">
           <input type="text" id="title" placeholder="Title" name="title" <?php echo "value=", $_SESSION["title"]?>><br><br>
           <textarea id="description" name="description" placeholder="Write anything here" <?php echo "value=", $_SESSION["description"]?>></textarea>
-          <br><br><button type="button" name="submit" onclick="checkempty()" value="submit">Submit</button>
+          <br><br><button type="submit" id="submit" name="submit" value="submit">Submit</button>
           <button type="button" onclick="resetText()" value="reset">Clear</button>
           <script>
 
@@ -40,40 +40,33 @@ session_start();
                 }
               }
 
-              function checkempty()
-              {
                 var title = document.getElementById("title");
                 var desc = document.getElementById("description");
-                var form = document.getElementById("enter");
+                var subbutton = document.getElementById("submit");
 
-                if (title.value.length == 0 || desc.value.length == 0)
-                {
-                  alert("Both fields have not been filled in");
-                  if (title.value.length == 0)
-                  {
-                    title.style.borderColor = "red";
-                  }
+                subbutton.addEventListener("click", function(e){
                   if (title.value.length > 0)
                   {
                     title.style.borderColor = "white";
                   }
-                  if (desc.value.length == 0)
+                  else
                   {
-                    desc.style.borderColor = "red";
+                    alert("Both fields have not been filled in");
+                    title.style.borderColor = "red";
+                    e.preventDefault();
                   }
                   if (desc.value.length > 0)
                   {
                     desc.style.borderColor = "white";
                   }
+                  else
+                  {
+                    alert("Both fields have not been filled in");
+                    desc.style.borderColor = "red";
+                    e.preventDefault();
+                  }
+                }, false);
 
-                  e.preventDefault();
-                 }
-                 else
-                 {
-                   form.submit();
-                 }
-
-               }
 
           </script>
     </<fieldset>
