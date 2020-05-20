bfad9970cc36655af65913dddbdcf208eda94e91
Hasib <ec19051@qmul.ac.uk>
2020-05-20 04:03:02 +0100

    changed

--- webroot/miniproject/viewBlog.php
+++ webroot/miniproject/viewBlog.php
@@ -73,7 +73,7 @@
 
            $result = mysqli_fetch_array($res2);
 
-           echo "<h3><style='font-family: Tahoma; text-align: center; color: white;'>", $result["title"], "</h3><p><style='color: white; font-family: Tahoma;'>", $result[description], "</p><p><br><small style='color:white; font-family:Tahoma;'>Posted: ", date("d/m/y g:i A", strtotime($result["datepost"])), "</small></p><hr>";
+           echo "<h3><style='font-family: Tahoma; text-align: center; color: white;'>", $result["title"], "</h3><p><style='color: white; font-family: Tahoma;'>", $result["description"], "</p><p><br><small style='color:white; font-family:Tahoma;'>Posted: ", date("d/m/y g:i A", strtotime($result["datepost"])), "</small></p><hr>";
          }
        }
        else
@@ -103,7 +103,7 @@
             }
             else
             {
-              echo "<h3><style='font-family: Tahoma; text-align: center; color: white;'>", $result["title"], "</h3><p><style='color: white; font-family: Tahoma;'>", $result[description], "</p><p><br><small style='color:white; font-family:Tahoma;'>Posted: ", date("d/m/y g:i A", strtotime($result["datepost"])), "</small></p><hr>";
+              echo "<h3><style='font-family: Tahoma; text-align: center; color: white;'>", $result["title"], "</h3><p><style='color: white; font-family: Tahoma;'>", $result["description"], "</p><p><br><small style='color:white; font-family:Tahoma;'>Posted: ", date("d/m/y g:i A", strtotime($result["datepost"])), "</small></p><hr>";
             }
           }
        }
